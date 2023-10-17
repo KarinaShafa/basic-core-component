ba5f5604d18f5967bd65f4bd5d3b4ed6c7d91788
KarinaShafa <karina_shafa_27rpl@student.smktelkom-mlg.sch.id>
2023-10-17 18:15:22 +0700

    modul 3

--- App.js
+++ App.js
@@ -1,39 +1,40 @@
 import { useRef, useState } from "react";
-import { View, DrawerLayoutAndroid,StatusBar } from "react-native";
+import { View, DrawerLayoutAndroid, StatusBar } from "react-native";
 import Header from "./components/header";
 import Button from "./components/button";
 import Separator from "./components/separator";
 import List from "./screens/list";
 import Article from "./screens/article";
 
-// functional component
+// Functional Component
 const App = () => {
-    // State declaration
-    const [page, setPage] = useState("list");
-    // reff declaratiion
-    const drawer = useRef(null);
-    // arrow function inside functional component
-    const changePage = (drawer, pageName) => {
-        // close drawer
-        drawer.current.closeDrawer();
-        // change state value
+  // State Declaration
+  const [page, setPage] = useState("list");
+  // Ref Declaration
+  const drawer = useRef(null);
 
-        setPage(pageName);
-    };
+  // Arrow Function inside Functional Component
+  const changePage = (drawer, pageName) => {
+    // Close Drawer
+    drawer.current.closeDrawer();
+    // Change state value
 
-    // arrow function inside functional component
-    const navigationView = () => (
-        <View style={{ padding: 30, backgroundColor: "#222222", flex: 1 }}>
+    setPage(pageName);
+  };
+
+  // Arrow Function inside Functional Component
+  const navigationView = () => (
+    <View style={{ padding: 30, backgroundColor: "#222222", flex: 1 }}>
       <Button text="List" onPress={() => changePage(drawer, "list")} />
       <Separator height={30} />
       <Button text="Article" onPress={() => changePage(drawer, "article")} />
       <Separator height={30} />
       <Button text="Close" onPress={() => drawer.current.closeDrawer()} />
     </View>
-    );
+  );
 
-    return (
-        <DrawerLayoutAndroid
+  return (
+    <DrawerLayoutAndroid
       ref={drawer}
       drawerWidth={300}
       drawerPosition="left"
@@ -45,7 +46,8 @@ const App = () => {
         {page == "list" ? <List /> : page == "article" ? <Article /> : null}
       </View>
     </DrawerLayoutAndroid>
-    );
+  );
 };
 
 export default App;
+
